5c4d7e4db22855153a790d4d0c6aba65ef1cc8c4
unknown <rajeev.sharma148@gmail.com>
2024-09-19 19:41:46 +0530

    MLOps assignmen-2

--- mlops-assignment2/HttpTrigger1/function_app.py
+++
@@ -1,7 +0,0 @@
-import azure.functions as func
-from __init__ import app as flask_app  # Import Flask app from __init__.py
-
-# The main function to handle requests and forward to Flask
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    """Azure function wrapper for the Flask application."""
-    return func.WsgiMiddleware(flask_app.wsgi_app).handle(req, context)
